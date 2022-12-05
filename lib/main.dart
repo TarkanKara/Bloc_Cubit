a7e069af69d979fd6f5ef7e69f5880d7d352dd68
Tarkan Kara <unnahmet23@hotmail.com>
2022-12-05 22:00:32 +0300

    Updated ThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.dark(),
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          headline5: TextStyle(
+            fontSize: 16,
+            color: Colors.black54,
+            fontWeight: FontWeight.w500,
+          ),
+          caption: TextStyle(
+              fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 1),
+        ),
+        primarySwatch: Colors.purple,
+        colorScheme: Theme.of(context)
+            .colorScheme
+            .copyWith(primary: Colors.purple, secondary: Colors.blueAccent),
+      ),
       title: 'Bloc && Cubit',
       //home:
     );
